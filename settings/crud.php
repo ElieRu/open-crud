441f3a2372ab50e53c82e2ac790173345db733f3
RUHAMYA Elie <elieruhamya20@gmail.com>
2024-03-19 13:20:56 +0200

    create functionality

--- settings/crud.php
+++ settings/crud.php
@@ -4,5 +4,19 @@ include_once 'settings.php';
 
 class Crud extends Settings
 {
-    // the crud methods...
+    public function create(array $datas)
+    {
+        $tmpAttributs = $this->attributs;
+        array_unshift($datas, null);
+        $prepAttributs = Params::magic($tmpAttributs, false);
+        $reqAttributs = Params::magic($tmpAttributs, true);
+
+        $save = function () use ($tmpAttributs, $prepAttributs, $reqAttributs, $datas) {
+
+            $request = $this->connection()->prepare("INSERT INTO {$this->table} ($prepAttributs) VALUES ($reqAttributs)");
+            return $request->execute(Params::request($tmpAttributs, $datas));
+        };
+
+        return $save();
+    }
 }
